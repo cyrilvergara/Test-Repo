4fc12da83d30ee750aef13880a5689a444ebe826
cyrilvergara <cyrilvergara@gmail.com>
2023-12-08 21:27:38 -0500

    merged all branches

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print('hello world')
 print('jello jorld')
 another test
+test again
 
 
 commands
@@ -9,3 +10,8 @@ git add . (all staging)
 git add filename.type (per file)
 git rm --cached filename
 git commit -m "first commit"
+
+branch
+git checkout -b new-feature
+git checkout master
+git branch
