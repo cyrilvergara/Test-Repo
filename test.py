1038e1686b12aea09541746768699c66ec1e6f73
cyrilvergara <cyrilvergara@gmail.com>
2023-12-08 21:13:42 -0500

    updated another.py and test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print('hello world')
+print('jello jorld')
+another test
+
+
+commands
+git status
+git add . (all staging)
+git add filename.type (per file)
+git rm --cached filename
+git commit -m "first commit"
